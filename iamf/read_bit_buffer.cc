8efb121183d469962ecd93373532382837e8f2eb
Googler <copybara-servicebot@google.com>
2024-02-20 23:22:29 -0500

    No public description
    PiperOrigin-RevId: 608835691

--- iamf/read_bit_buffer.cc
+++ iamf/read_bit_buffer.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2024, Alliance for Open Media. All rights reserved
  *
  * This source code is subject to the terms of the BSD 3-Clause Clear License
  * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
@@ -16,9 +16,32 @@
 #include <vector>
 
 #include "absl/status/status.h"
+#include "iamf/bit_buffer_util.h"
 #include "iamf/cli/leb_generator.h"
+#include "iamf/ia.h"
 
 namespace iamf_tools {
+
+namespace {
+
+bool ShouldRead(const int64_t& source_offset,
+                const std::vector<uint8_t>& source,
+                const int32_t& remaining_bits_to_read) {
+  const bool valid_bit_offset = (source_offset / 8) < source.size();
+  const bool bits_to_read = remaining_bits_to_read > 0;
+  return valid_bit_offset && bits_to_read;
+}
+
+// Reads one bit from data at position `source_offset`. Reads in order of most
+// significant to least significant - that is, offset = 0 refers to the bit in
+// position 2^7, offset = 1 refers to the bit in position 2^6, etc. Caller
+// should ensure that source_offset/8 is < source.size().
+uint8_t GetUpperBit(int64_t source_offset, std::vector<uint8_t>* source) {
+  int64_t byteIndex = source_offset / 8;
+  uint8_t bitIndex = 7 - (source_offset % 8);
+  return (source->at(byteIndex) >> bitIndex) & 0x01;
+}
+}  // namespace
 
 ReadBitBuffer::ReadBitBuffer(int64_t capacity, std::vector<uint8_t>* source,
                              const LebGenerator& leb_generator)
@@ -27,24 +50,51 @@ ReadBitBuffer::ReadBitBuffer(int64_t capacity, std::vector<uint8_t>* source,
 }
 
 // Reads n = `num_bits` bits from the buffer. These are the lower n bits of
-// bit_buffer_. n must be <= 64. The read data is consumed, meaning
-// bit_buffer_offset_ is incremented by n as a side effect of this fxn.
+// `bit_buffer_`. n must be <= 64. The read data is consumed, meaning
+// `bit_buffer_offset_` is incremented by n as a side effect of this fxn.
 absl::Status ReadBitBuffer::ReadUnsignedLiteral(uint64_t* data, int num_bits) {
   return absl::UnimplementedError("Not yet implemented.");
 }
 
 // Loads enough bits from source such that there are at least n =
-// `required_num_bits` in bit_buffer_ after completion. Returns an error if
-// there are not enough bits in source_ to fulfill this request. If source_
+// `required_num_bits` in `bit_buffer_` after completion. Returns an error if
+// there are not enough bits in `source_` to fulfill this request. If `source_`
 // contains enough data, this function will fill the read buffer completely.
 absl::Status ReadBitBuffer::LoadBits(const int32_t required_num_bits) {
-  return absl::UnimplementedError("Not yet implemented.");
+  DiscardAllBits();
+  int original_source_offset = source_bit_offset_;
+  int remaining_bits_to_load = required_num_bits;
+  int64_t bit_buffer_write_offset = 0;
+  while (ShouldRead(source_bit_offset_, *source_, remaining_bits_to_load) &&
+         (bit_buffer_.size() != bit_buffer_.capacity())) {
+    if (remaining_bits_to_load < 8 || source_bit_offset_ % 8 != 0) {
+      // Load bit by bit
+      uint8_t loaded_bit = GetUpperBit(source_bit_offset_, source_);
+      RETURN_IF_NOT_OK(
+          CanWriteBits(true, 1, bit_buffer_write_offset, bit_buffer_));
+      RETURN_IF_NOT_OK(
+          WriteBit(loaded_bit, bit_buffer_write_offset, bit_buffer_));
+      bit_buffer_write_offset = bit_buffer_write_offset % 8;
+      source_bit_offset_++;
+      remaining_bits_to_load--;
+    } else {
+      // Load byte by byte
+      bit_buffer_.push_back(source_->at(source_bit_offset_ / 8));
+      source_bit_offset_ += 8;
+      remaining_bits_to_load -= 8;
+    }
+  }
+  if (remaining_bits_to_load != 0) {
+    source_bit_offset_ = original_source_offset;
+    DiscardAllBits();
+    return absl::ResourceExhaustedError("Not enough bits in source.");
+  }
+  return absl::OkStatus();
 }
 
-absl::Status ReadBitBuffer::DiscardAllBits() {
-  bit_buffer_offset_ = 0;
+void ReadBitBuffer::DiscardAllBits() {
+  buffer_bit_offset_ = 0;
   bit_buffer_.clear();
-  return absl::OkStatus();
 }
 
 }  // namespace iamf_tools
--- iamf/tests/read_bit_buffer_test.cc
+++ iamf/tests/read_bit_buffer_test.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2024, Alliance for Open Media. All rights reserved
  *
  * This source code is subject to the terms of the BSD 3-Clause Clear License
  * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
@@ -15,25 +15,65 @@
 #include <memory>
 #include <vector>
 
+#include "absl/status/status.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace iamf_tools {
+using absl::StatusCode::kResourceExhausted;
+using testing::UnorderedElementsAreArray;
 namespace {
 
 class ReadBitBufferTest : public ::testing::Test {
  public:
-  std::vector<uint8_t> rb_data_;
+  std::vector<uint8_t> source_data_;
   int64_t rb_capacity_;
   std::unique_ptr<ReadBitBuffer> CreateReadBitBuffer() {
-    return std::make_unique<ReadBitBuffer>(rb_capacity_, &rb_data_);
+    return std::make_unique<ReadBitBuffer>(rb_capacity_, &source_data_);
   }
 };
 
 TEST_F(ReadBitBufferTest, ReadBitBufferConstructor) {
-  rb_data_ = {};
+  source_data_ = {};
   rb_capacity_ = 0;
   std::unique_ptr<ReadBitBuffer> rb_ = CreateReadBitBuffer();
   EXPECT_NE(rb_, nullptr);
+}
+
+TEST_F(ReadBitBufferTest, LoadBitsByteAligned) {
+  source_data_ = {0x09, 0x02, 0xab};
+  rb_capacity_ = 1024;
+  std::unique_ptr<ReadBitBuffer> rb_ = CreateReadBitBuffer();
+  EXPECT_TRUE(rb_->LoadBits(24).ok());
+  EXPECT_THAT(rb_->bit_buffer(), UnorderedElementsAreArray(source_data_));
+}
+
+TEST_F(ReadBitBufferTest, LoadBitsNotByteAligned) {
+  source_data_ = {0b10100001};
+  rb_capacity_ = 1024;
+  std::unique_ptr<ReadBitBuffer> rb_ = CreateReadBitBuffer();
+  EXPECT_TRUE(rb_->LoadBits(3).ok());
+  // Only read the first 3 bits (101) - the rest of the bits in the byte are
+  // zeroed out.
+  std::vector<uint8_t> expected = {0b10100000};
+  EXPECT_THAT(rb_->bit_buffer(), UnorderedElementsAreArray(expected));
+  EXPECT_EQ(rb_->source_bit_offset(), 3);
+  // Load bits again. This will clear the buffer while still reading from the
+  // updated source offset.
+  EXPECT_TRUE(rb_->LoadBits(5).ok());
+  expected = {
+      0b00001000};  // {00001} these bits are loaded from the 5 remaining bits
+                    // in the buffer - the rest of the bits are zeroed out.
+  EXPECT_THAT(rb_->bit_buffer(), UnorderedElementsAreArray(expected));
+  EXPECT_EQ(rb_->source_bit_offset(), 8);
+}
+
+TEST_F(ReadBitBufferTest, LoadBitsNotEnoughSourceBits) {
+  source_data_ = {0x09, 0x02, 0xab};
+  rb_capacity_ = 1024;
+  std::unique_ptr<ReadBitBuffer> rb_ = CreateReadBitBuffer();
+  EXPECT_EQ(rb_->LoadBits(32).code(), kResourceExhausted);
+  EXPECT_EQ(rb_->bit_buffer().size(), 0);
 }
 
 }  // namespace
