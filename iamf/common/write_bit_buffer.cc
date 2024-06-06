c936e21f36963d24668c727c46ba760d76ec9c83
Joseph Cullen <jwcullen@google.com>
2024-06-06 14:43:43 -0400

    Update write_bit_buffer.cc

--- iamf/common/write_bit_buffer.cc
+++ iamf/common/write_bit_buffer.cc
@@ -180,7 +180,7 @@ absl::Status WriteBitBuffer::WriteString(const std::string& data) {
   for (int i = 0; i < kIamfMaxStringSize; i++) {
     // Note that some systems have `char` as signed and others unsigned. Write
     // the same raw byte value regardless.
-    const uint8_t byte = std::bit_cast<uint8_t>(data[i]);
+    const uint8_t byte = std::static_cast<uint8_t>(data[i]);
     RETURN_IF_NOT_OK(WriteUnsignedLiteral(byte, 8));
 
     // Exit successfully after last byte was written.
