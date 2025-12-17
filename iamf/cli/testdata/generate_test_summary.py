86ea490003566c906631ad5a1a3270077aff7ddf
Joseph Cullen <jwcullen@google.com>
2025-12-17 10:02:56 -0500

    Refactor test summary script for improved clarity

--- iamf/cli/testdata/generate_test_summary.py
+++ iamf/cli/testdata/generate_test_summary.py
@@ -1,10 +1,11 @@
 """Generates a summary of .textprotos."""
 
+import collections
+from collections.abc import Sequence
 import csv
 from glob import glob
 import os
 import re
-from typing import Sequence
 
 from absl import app
 from absl import flags
@@ -14,7 +15,7 @@ from google.protobuf import text_format
 from iamf.cli.proto import user_metadata_pb2
 
 
-_test_repository_tag = flags.DEFINE_string(
+_TEST_REPOSITORY_TAG = flags.DEFINE_string(
     'test_repository_tag',
     'github/aomediacodec/libiamf/main',
     'Used to determine which test vectors to include in the summary or'
@@ -27,6 +28,11 @@ _TEST_DATA_DIR = flags.DEFINE_string(
     'Path to the directory containing test data files.',
     required=True,
 )
+flags.register_validator(
+    'test_data_dir',
+    lambda value: os.path.isdir(value) and os.path.exists(value),
+    message='--test_data_dir must be a valid, existing directory.',
+)
 
 _OUTPUT_DIR = flags.DEFINE_string(
     'output_dir',
@@ -35,7 +41,7 @@ _OUTPUT_DIR = flags.DEFINE_string(
     required=True,
 )
 
-SPEC_SECTIONS = [
+SPEC_SECTIONS = (
     '3.1',
     '3.2',
     '3.3',
@@ -96,31 +102,33 @@ SPEC_SECTIONS = [
     '9.1.2.2',
     '9.1.2.3',
     '9.1.2.4',
-]
+)
 
 
-def pretty_print(string):
+def pretty_print(string) -> str:
   """Removes enclosing brackets and quotes in the printed string."""
   return re.sub(r'\[|\]|{|}|\'', '', str(string))
 
 
 def main(argv: Sequence[str]) -> None:
-  del argv
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
 
   os.makedirs(_OUTPUT_DIR.value, exist_ok=True)
 
-  test_repository_tag = _test_repository_tag.value
+  test_repository_tag = _TEST_REPOSITORY_TAG.value
   test_proto_files = sorted(
       glob(os.path.join(_TEST_DATA_DIR.value, '*.textproto'))
   )
   summary = {}
-  # spec_coverage is {'section': {'section_field': [test1, test2]}}
-  spec_coverage = {key: {} for key in SPEC_SECTIONS}
+  # spec_coverage is {'section': {'section_field': [test1, test2, ...]}}
+  spec_coverage = collections.defaultdict(lambda: collections.defaultdict(list))
   for test_proto_file in test_proto_files:
     logging.info('Found %s', test_proto_file)
-    user_metadata_proto = user_metadata_pb2.UserMetadata()
     with open(test_proto_file, 'r') as f:
-      text_format.Parse(f.read(), user_metadata_proto)
+      user_metadata_proto = text_format.ParseLines(
+          f, user_metadata_pb2.UserMetadata()
+      )
       # Filter out test vectors which do not have a matching tag, but allow all
       # when the tag is empty.
       if test_repository_tag and (
@@ -146,13 +154,7 @@ def main(argv: Sequence[str]) -> None:
       for tested_section in tested_sections:
         # The sections are in the format "x.x" or "x.x/field".
         [section, _, field] = tested_section.partition('/')
-        if section in spec_coverage:
-          if field in spec_coverage[section]:
-            spec_coverage[section][field].append(prefix)
-          else:
-            spec_coverage[section][field] = [prefix]
-        else:
-          spec_coverage[section] = {field: [prefix]}
+        spec_coverage[section][field].append(prefix)
 
   # Generate CSV.
   with open(
@@ -187,11 +189,11 @@ def main(argv: Sequence[str]) -> None:
     # Split the section label by '.' and sort by comparing each part
     # incrementally.
     sorted_sections = sorted(
-        spec_coverage.keys(),
+        SPEC_SECTIONS,
         key=lambda x: [int(subsection) for subsection in x.split('.')],
     )
     for section in sorted_sections:
-      if spec_coverage[section]:
+      if section in spec_coverage:
         # Sort by field alphabetically
         sorted_fields = sorted(spec_coverage[section])
         for field in sorted_fields:
